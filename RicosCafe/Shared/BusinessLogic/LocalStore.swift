c6bd973853675558b02d6ea2313a29a59b1db2e0
Bruce McTigue <bmctigue@tiguer.com>
2018-12-28 10:03:25 -0700

    Moved local store to shared business logic

---
+++ RicosCafe/Shared/BusinessLogic/LocalStore.swift
@@ -0,0 +1,27 @@
+//
+//  DrinksLocalStore.swift
+//  RicosCafe
+//
+//  Created by Bruce McTigue on 12/27/18.
+//  Copyright © 2018 tiguer. All rights reserved.
+//
+
+import UIKit
+
+final class LocalStore: StoreProtocol {
+    
+    private var assetName: String
+    
+    init(_ assetName: String) {
+        self.assetName = assetName
+    }
+    
+    func fetchData(completionHandler: @escaping (Data?) -> Void) {
+        if let asset = NSDataAsset(name: assetName, bundle: Bundle.main) {
+            completionHandler(asset.data)
+        } else {
+            print("json asset couldn't be loaded")
+            completionHandler(nil)
+        }
+    }
+}
