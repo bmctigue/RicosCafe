872c86304fcf907a1c6e72a02b2170368e0b0454
Bruce McTigue <bmctigue@tiguer.com>
2018-12-29 11:32:46 -0700

    Refactoring and simplifying the app builder.

--- RicosCafe/App/AppBuilder.swift
+++ RicosCafe/App/AppBuilder.swift
@@ -11,28 +11,14 @@ import UIKit
 enum Builder {
     final class App: BaseBuilder {
         
-        private static let assetName = "drinksJson"
-        
-        private static let drinksImageName = "drinks"
-        private static let foodImageName = "food"
-        private static let dessertImageName = "dessert"
-        
-        private static let drinksTitle = "Drinks"
-        private static let foodTitle = "Food"
-        private static let dessertTitle = "Dessert"
-        
         private let store = LocalStore(assetName)
-        private let drinksBuilder: TabBuilder
         
         private var window: UIWindow?
         private (set) var tabBarBuilder: Builder.TabBar!
-        private var builders: [TabBuilder]
         
         init(with window: UIWindow?) {
             self.window = window
-            self.drinksBuilder = Drinks.Builder(with: Builder.App.drinksImageName, title: Builder.App.drinksTitle, store: store)
-            self.builders = [drinksBuilder]
-            self.tabBarBuilder = Builder.TabBar(with: builders)
+            self.tabBarBuilder = Builder.TabBar(with: configureBuilders())
         }
         
         func run() {
@@ -40,5 +26,22 @@ enum Builder {
             self.window?.rootViewController = tabBarBuilder.getTabBar()
             self.window?.makeKeyAndVisible()
         }
+    }
+}
+
+extension Builder.App {
+    private static let assetName = "drinksJson"
+    
+    private static let drinksImageName = "drinks"
+    private static let foodImageName = "food"
+    private static let dessertImageName = "dessert"
+    
+    private static let drinksTitle = "Drinks"
+    private static let foodTitle = "Food"
+    private static let dessertTitle = "Dessert"
+    
+    func configureBuilders() -> [TabBuilder] {
+        let drinksBuilder = Drinks.Builder(with: Builder.App.drinksImageName, title: Builder.App.drinksTitle, store: store)
+        return [drinksBuilder]
     }
 }
